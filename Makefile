20bb6ddbf6a150a0b8be172ebdbc065b25685145
Eric Huang <47696422+ehuan2@users.noreply.github.com>
2021-09-18 13:30:47 -0400

    Auth Stuff (#1)
    * Starting auth
    
    * Got basic auth working
    
    * Auth all setup
    
    * Stuff

--- Makefile
+++ Makefile
@@ -7,5 +7,11 @@ build:
 run:
 	docker-compose up
 
+run-db:
+	docker-compose -f docker-compose.db.yml up -d db redis 
+
+db-psql:
+	docker exec -it backend_db_1 /bin/sh
+
 push:
 	docker push ehuan2/htn-rabbithole:latest
---
+++ src/services/db.service.js
@@ -0,0 +1,40 @@
+const {Client} = require("pg");
+
+const postgresClient = new Client({
+    connectionString: process.env.POSTGRES_CONNECTION_STRING
+})
+
+module.exports.migrations = async () => { 
+    let migrationsToDo = 
+        `CREATE TABLE users (
+            id serial primary key,
+            username varchar(64) not null unique,
+            password varchar(128) not null
+        )`
+
+    postgresClient.query(migrationsToDo).then(_ => null).catch(err => err)
+}
+
+module.exports.postgresConnect = async () => {
+    console.log(process.env.POSTGRES_CONNECTION_STRING)
+    return postgresClient.connect()
+        .then(() => console.log("connected to psql"))
+        .catch(err => console.error(err))
+}
+
+module.exports.dbRegisterUser = async(username, password) => 
+    postgresClient.query(
+        'INSERT INTO users(username, password) VALUES ($1, $2) RETURNING id',
+        [username, password]
+    )
+    .then((res) => [null, res.rows[0].id])
+    .catch(err => [err, null])
+
+module.exports.getUserByUsername = async (username) =>
+    postgresClient.query(
+        'SELECT * FROM users WHERE username = $1',
+        [username]
+    )
+    .then((res) => [null, res.rows])
+    .catch(err => [err, null])
+
